7e2ec78a509d62dcd1c8b187eca64a887daecb06
MELDemy <mahmoudeldemerdash5@gmail.com>
2024-08-11 17:09:00 +0300

    initiate store app

---
+++ learning/store_app/android/app/src/main/kotlin/com/eldemy/store_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.eldemy.store_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ learning/store_app/lib/models/product_model.dart
@@ -0,0 +1,10 @@
+class ProductModel {
+  int? id;
+  String? title;
+  double? price;
+  String? description;
+  String? category;
+  String? image;
+  Map<String, dynamic>? rating; //
+  // : {"rate": 3.9,"count": 120}
+}
