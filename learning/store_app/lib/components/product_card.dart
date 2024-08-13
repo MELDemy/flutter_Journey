47c17db39c53fa2f21ffd966f05100c483824749
MELDemy <mahmoudeldemerdash5@gmail.com>
2024-08-13 04:03:14 +0300

    Home screen UI

--- learning/store_app/lib/components/product_card.dart
+++ learning/store_app/lib/components/product_card.dart
@@ -1,48 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:store_app/models/product_model.dart';
 
 class ProductCard extends StatelessWidget {
-  ProductCard({super.key});
-  ProductModel product = ProductModel(
-    id: 7,
-    title: "Hand bag",
-    price: 350.5,
-    description: "description",
-    category: "Bags",
-    image: "https://i.pravatar.cc",
-  );
+  ProductCard({required this.product, super.key});
+  ProductModel product;
+  // ProductModel(
+  //   id: 7,
+  //   title: "Hand bag",
+  //   price: 350.5,
+  //   description: "description",
+  //   category: "Bags",
+  //   image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  // );
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: 100,
-      child: Card(
-        elevation: 20,
-        child: Column(
-          children: [
-            // Image.network(product.image),
-            Text(
-              product.title,
-              style: TextStyle(
-                color: Colors.grey,
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          child: Card(
+            surfaceTintColor: Colors.white,
+            shadowColor: Colors.white,
+            elevation: 5,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    product.title.split(' ').take(4).join(' '),
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '\$${product.price}',
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      IconButton(
+                          padding: EdgeInsets.zero,
+                          constraints: BoxConstraints(),
+                          style: const ButtonStyle(
+                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                          ),
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.favorite,
+                            color: Colors.grey,
+                          ))
+                    ],
+                  )
+                ],
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('\$${product.price}'),
-                IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      FontAwesomeIcons.solidHeart,
-                      color: Colors.grey,
-                    ))
-              ],
-            )
-          ],
+          ),
         ),
-      ),
+        Positioned(
+          top: -55,
+          right: 20,
+          child: Image.network(
+            product.image,
+            height: 100,
+            width: 100,
+          ),
+        ),
+      ],
     );
   }
 }
--- learning/store_app/lib/screens/home_screen.dart
+++ learning/store_app/lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:store_app/components/product_card.dart';
+import 'package:store_app/models/product_model.dart';
+import 'package:store_app/services/get_all_products_service.dart';
 
 class HomeScreen extends StatelessWidget {
   static String id = 'HomeScreen';
@@ -20,7 +22,31 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: ProductCard(),
+      body: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 50, 20, 0),
+          child: FutureBuilder<List<ProductModel>>(
+            future: AllProductsService().getAllProducts(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<ProductModel> products = snapshot.data!;
+                return GridView.builder(
+                  itemCount: products.length,
+                  clipBehavior: Clip.none,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 70,
+                    crossAxisSpacing: 20,
+                    childAspectRatio: 1.285,
+                  ),
+                  itemBuilder: (context, index) {
+                    return ProductCard(product: products[index]);
+                  },
+                );
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          )),
     );
   }
 }
