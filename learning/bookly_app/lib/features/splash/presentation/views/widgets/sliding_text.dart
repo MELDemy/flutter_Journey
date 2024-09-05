ad425d68c5aad71128bf2ba6975874041dc85bc0
MELDemy <mahmoudeldemerdash5@gmail.com>
2024-09-05 21:12:34 +0300

    Create splash view with animation

---
+++ learning/bookly_app/lib/features/splash/presentation/views/widgets/sliding_text.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class SlidingText extends StatefulWidget {
+  const SlidingText({
+    super.key,
+  });
+
+  @override
+  State<SlidingText> createState() => _SlidingTextState();
+}
+
+class _SlidingTextState extends State<SlidingText>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: slidingAnimation,
+        builder: (context, _) {
+          return SlideTransition(
+            position: slidingAnimation,
+            child: const Text(
+              "Read Free Books",
+              textAlign: TextAlign.center,
+            ),
+          );
+        });
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
+  }
+}
